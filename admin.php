a3571b84947e59ba02fb479df8945e972dd74906
wangzhicong <31824066+wangzhicong@users.noreply.github.com>
2017-11-21 12:33:45 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
 include_once('csrf.php');
@@ -19,26 +18,3 @@ session_start();
 
         <input type="submit" value="Login"/>
     </form>
-=======
-
-<?php
-include_once('csrf.php');
-session_start();
-?>
-
-<fieldset>
-    <legend>Login</legend>
-    <form id="form" method="GET" action="auth-process.php" enctype="multipart/form-data">
-
-        <label for="prod_name">Email *</label>
-        <div><input id="email" name="em" required="true" pattern="^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+""/></div>
-
-        <label for="prod_price">password *</label>
-        <div><input id="password" name="password" required="true" pattern="^[\d]+$" /></div>
-        <input  id="nonce" type="hidden" name = "action" value="login"/>
-        <input  id="nonce" type="hidden" name = "nonce" value="<?php echo getNonce('login');?>"/>
-
-        <input type="submit" value="Login"/>
-    </form>
->>>>>>> 2bea1ca962ad3462eed98d20520659aba74bdd9f
-</fieldset>
